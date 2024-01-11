f2d803e92ecd62e2244b94dd30ca436b8d4d0cff
azheng606 <144460945+azheng606@users.noreply.github.com>
2024-01-11 10:14:32 -0500

    shr

---
+++ main.js
@@ -0,0 +1,9 @@
+const DOMSelectors = {
+        box:document.querySelector(".box"),
+        form: document.querySelector("#form"),
+        start: document.querySelector("#start"),
+        category:document.querySelector(".category"),
+        place:document.querySelector(".place"),
+}
+
+const NAMEURL = "https://www.themealdb.com/api/json/v1/1/search.php?s="
