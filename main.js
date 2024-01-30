7d706e8b48b5ba0de75af1a5047728c8d8c15583
azheng606 <144460945+azheng606@users.noreply.github.com>
2024-01-30 10:16:04 -0500

    y

--- main.js
+++ main.js
@@ -19,42 +19,6 @@ const same = [];
                 } 
                 const all = await response.json ();
 
-<<<<<<< HEAD
-const NAMEURL = "https://www.themealdb.com/api/json/v1/1/search.php?s="
-        
-
-            async function getData() {
-                try {
-                    const response = await fetch(NAMEURL);
-                    if (response.status != 200){
-                        throw new Error(response.statusText);
-                    }
-                    const all = await response.json ();
-                    console.log (all);
-                    const lolz = all.meals.filter((meals)=>meals.strArea === "Vegetarian")
-                    lolz.forEach((el)=> { DOMSelectors.box.insertAdjacentHTML("beforeend", `
-                    <div class="container">
-                    <div class="card">
-                    <div class="front">
-                    <div class ="name"> ${el.strMeal}</div>
-                    </div>
-                    <div class="back">
-                    <div class ="shortfilms">  ${"Meal: "+ el.strMeal} </div>
-                   </div>
-                    </div></div>`)})
-                    
-                    
-                  } 
-            catch (error) {
-            console.log(error, "UH OH");
-            DOMSelectors.awman.textContent = "Oopsies nothing fouund ;)";   
-            };
-            } 
-            getData(NAMEURL)
-            
-             
-        
-=======
               for (let i = 0; i < all.meals.length; i++) {
                 const each = all.meals [i]
                 if (each.strCategory.toLowerCase() === categoryinput.toLowerCase() ) {
@@ -122,4 +86,3 @@ function clearfields () {
 function clearvalue () { 
         DOMSelectors.category.value= "";
 }
->>>>>>> 542c0517209e476d2e75e3eff2dbd71076f5334d
