33f15b52d0323da70004e3389354957c8d4f1b9b
azheng606 <144460945+azheng606@users.noreply.github.com>
2024-01-30 10:04:05 -0500

    k

--- main.js
+++ main.js
@@ -7,3 +7,36 @@ const DOMSelectors = {
 }
 
 const NAMEURL = "https://www.themealdb.com/api/json/v1/1/search.php?s="
+        
+
+            async function getData() {
+                try {
+                    const response = await fetch(NAMEURL);
+                    if (response.status != 200){
+                        throw new Error(response.statusText);
+                    }
+                    const all = await response.json ();
+                    console.log (all);
+                    const lolz = all.meals.filter((meals)=>meals.strArea === "Vegetarian")
+                    lolz.forEach((el)=> { DOMSelectors.box.insertAdjacentHTML("beforeend", `
+                    <div class="container">
+                    <div class="card">
+                    <div class="front">
+                    <div class ="name"> ${el.strMeal}</div>
+                    </div>
+                    <div class="back">
+                    <div class ="shortfilms">  ${"Meal: "+ el.strMeal} </div>
+                   </div>
+                    </div></div>`)})
+                    
+                    
+                  } 
+            catch (error) {
+            console.log(error, "UH OH");
+            DOMSelectors.awman.textContent = "Oopsies nothing fouund ;)";   
+            };
+            } 
+            getData(NAMEURL)
+            
+             
+        
